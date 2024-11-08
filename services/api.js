de9ebe6c44fe1ac0c46c5b9300f7ac17b8acafb4
Lucas Mazo <lucasmazo@gmail.com>
2024-11-07 21:06:11 -0300

    finalizado

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://puxtarrtmgbcgurxnbfr.supabase.co/rest/v1/',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1eHRhcnJ0bWdiY2d1cnhuYmZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEwMjI0NTEsImV4cCI6MjA0NjU5ODQ1MX0.Jr-bYdRldoKEPWzMQ4StQa6Dr36irFc38QwlKBqiBqE",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1eHRhcnJ0bWdiY2d1cnhuYmZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEwMjI0NTEsImV4cCI6MjA0NjU5ODQ1MX0.Jr-bYdRldoKEPWzMQ4StQa6Dr36irFc38QwlKBqiBqE"
     }
 })
